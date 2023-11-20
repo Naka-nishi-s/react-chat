fb6ed47ed69599cf28522235b09368095104b1eb
Hyodo <90432430+Naka-nishi-s@users.noreply.github.com>
2023-11-20 14:17:24 +0900

    良い感じ

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { Box, Grid } from "@mui/material";
+import { Chat } from "./Chat";
+
+function App() {
+  return (
+    <Grid container spacing={2}>
+      {/* 左側の画像は、画面幅がsm以上の時にのみ表示 */}
+      <Grid
+        item
+        sm={6}
+        xs={0}
+        style={{
+          backgroundImage: "url(あなたの画像URL)",
+          backgroundSize: "cover",
+        }}
+      >
+        {/* 左側のコンテンツ（画像など） */}
+      </Grid>
+      {/* チャット部分は常に表示 */}
+      <Grid item xs={12} sm={6}>
+        <Box sx={{ p: 2 }}>
+          <Chat />
+        </Box>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default App;
---
+++ src/Chat.tsx
@@ -0,0 +1,103 @@
+import { keyframes } from "@emotion/react";
+import { Avatar, Box, Button, Grid, TextField } from "@mui/material";
+import { useEffect, useState } from "react";
+
+type Message = {
+  sender: "bot" | "user";
+  text: string;
+};
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+`;
+
+export const Chat = () => {
+  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [input, setInput] = useState<string>("");
+  const [chatHistory, setChatHistory] = useState<Message[]>([]);
+  const questions: string[] = ["ユーザー名は？", "パスワードは？"];
+
+  useEffect(() => {
+    if (currentStep < questions.length) {
+      setTimeout(() => {
+        setChatHistory((chatHistory) => [
+          ...chatHistory,
+          { sender: "bot", text: questions[currentStep] },
+        ]);
+      }, 500); // 500ミリ秒の遅延
+    }
+  }, [currentStep]);
+
+  const handleSend = () => {
+    setChatHistory((chatHistory) => [
+      ...chatHistory,
+      { sender: "user", text: input },
+    ]);
+    setInput("");
+
+    if (currentStep < questions.length - 1) {
+      setCurrentStep(currentStep + 1);
+    } else {
+      console.log("会話終了");
+    }
+  };
+
+  return (
+    <Box sx={{ p: 2 }}>
+      {chatHistory.map((message, index) => (
+        <Grid
+          container
+          key={index}
+          justifyContent={message.sender === "user" ? "flex-end" : "flex-start"}
+        >
+          <Grid item>
+            {message.sender === "bot" && (
+              <Avatar alt="Bot" src="/bot-icon.png" />
+            )}
+            <Box
+              sx={{
+                margin: 1,
+                padding: 1,
+                border: "1px solid grey",
+                borderRadius: "10px",
+                animation: `${fadeIn} 0.5s ease`,
+              }}
+            >
+              <p>{message.text}</p>
+            </Box>
+            {message.sender === "user" && (
+              <Avatar alt="User" src="/user-icon.png" />
+            )}
+          </Grid>
+        </Grid>
+      ))}
+      <Grid container spacing={1}>
+        <Grid item xs>
+          <TextField
+            fullWidth
+            variant="outlined"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            disabled={currentStep >= questions.length}
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            onClick={handleSend}
+            disabled={currentStep >= questions.length}
+          >
+            Send
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
